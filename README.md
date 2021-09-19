4bcf65b9f1668c04573b05557551f8dfcc418709
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-09-19 20:33:11 +0800

    For checking - connection refused error.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pysocket-flutter
 Socket IO Python Server and Flutter Demo
+
+### Server
+* Go to the server directory.
+* Install the dependencies using pip.
+* Run the server: ```gunicorn --threads 50 app:app```
---
+++ client/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    IO.Socket socket = IO.io(
+        'http://192.168.254.103:8000/',
+        IO.OptionBuilder()
+            .setTransports(['websocket']) // for Flutter or Dart VM            
+            .build());
+    socket.onConnect((_) {
+      print('connect');
+      socket.emit('msg', 'test');
+    });
+
+    //When an event recieved from server, data is added to the stream
+    socket.on('event', (data) => print(data.toString()));
+    socket.onDisconnect((_) => print('disconnect'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text("Socket IO Client"),
+      ),
+    );
+  }
+}
